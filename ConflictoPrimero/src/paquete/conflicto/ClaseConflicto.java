48272c58e53d55d24ccb92390d1cd24fe56fd4db
21836404 <21836404@N205-24.alumnos.uem.es>
2019-01-23 09:36:31 +0100

    Conflicto

---
+++ ConflictoPrimero/src/paquete/conflicto/ClaseConflicto.java
@@ -0,0 +1,14 @@
+package paquete.conflicto;
+
+public class ClaseConflicto {
+
+	public static void main(String[] args) {
+		String[] colores = { "rojo", "morado", "azul", "verde", 
+				"amarillo", "naranja", "rojo" };
+		for (int i = 0; i < colores.length; i++) {
+			System.out.println(colores[i]);
+		}
+
+	}
+
+}
